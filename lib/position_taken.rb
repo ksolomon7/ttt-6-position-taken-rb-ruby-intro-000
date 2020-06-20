c3f553c8b3a01fdce14a6734ffcc27b1158ba50b
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-06-20 17:37:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
     false
   elsif board[index]==""
     false
-  elsif board[index]== nil 
+  elsif board[index]== nil
     false
   elsif board[index]== "X" || board[index]== "O"
     true
